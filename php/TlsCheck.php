ad1fffc655f8b0fd8e62f5959fd560aabce57cab
Jay <japatel@paypal.com>
2016-09-27 15:57:50 -0500

    Updated TlsCheck.php

--- php/TlsCheck.php
+++ php/TlsCheck.php
@@ -5,11 +5,12 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://tlstest.paypal.com/"); 
 curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
 
-var_dump(curl_exec($ch));
+curl_exec($ch);
+echo "\n";
 
 if ($err = curl_error($ch)) {
-var_dump($err);
-echo "DEBUG INFORMATION:\n###########";
-echo "CURL VERSION";
-echo json_encode(curl_version(), JSON_PRETTY_PRINT);
+	var_dump($err);
+	echo "DEBUG INFORMATION:\n###########\n";
+	echo "CURL VERSION:\n";
+	echo json_encode(curl_version(), JSON_PRETTY_PRINT);
 }
