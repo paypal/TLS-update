3dbd4e4338ecb37669678dbba653ed48da49b5bc
Rashid-rc <59302090+Rashid-rc@users.noreply.github.com>
2019-12-28 23:08:50 +0400

    Update TlsCheck.php
    https://github.com/paypal/TLS-update/blob/6b5f7ea3e2ee3810aac624a0689c950cc7a6bd3e/php/TlsCheck.php#L3

--- php/TlsCheck.php
+++ php/TlsCheck.php
@@ -1,19 +1,2 @@
-<?php
 
-$ch = curl_init(); 
 
-curl_setopt($ch, CURLOPT_URL, "https://tlstest.paypal.com/"); 
-curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
-
-// Some environments may be capable of TLS 1.2 but it is not in their list of defaults so need the SSL version option to be set.
-curl_setopt($ch, CURLOPT_SSLVERSION, 6);
-
-curl_exec($ch);
-echo "\n";
-
-if ($err = curl_error($ch)) {
-	var_dump($err);
-	echo "DEBUG INFORMATION:\n###########\n";
-	echo "CURL VERSION:\n";
-	echo json_encode(curl_version(), JSON_PRETTY_PRINT);
-}
